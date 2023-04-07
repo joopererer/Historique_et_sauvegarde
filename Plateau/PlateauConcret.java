876743d72c150fe8b96554b5a511376b9cf3af0d
Jiawei LI <52564125+joopererer@users.noreply.github.com>
2023-04-07 20:56:25 +0200

    Add files via upload
    Code initial

---
+++ Plateau/PlateauConcret.java
@@ -0,0 +1,12 @@
+package Plateau;
+
+import Plateau.PlateauAbstrait;
+
+public class PlateauConcret extends PlateauAbstrait {
+	public PlateauConcret(int i, int j) {
+		super(i,j);
+	}
+
+	public PlateauConcret(String fichier) {
+	}
+}
